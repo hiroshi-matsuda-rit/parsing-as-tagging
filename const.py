927f8767c4874f9905f58ab76a8579aadb19ecc3
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-03-19 18:39:32 +0900

    arrange const.py

--- const.py
+++ const.py
@@ -25,8 +25,10 @@ POL = "Polish"
 SWE = "swedish"
 ENG = "English"
 LANG = [BAQ, CHN, CHN09, FRE, GER, HEB, HUN, KOR, POL, SWE, ENG,
-        "bg","ca","cs","de","en","es", "fr","it","nl","no","ro","ru"]
+        "en","ja","zh","ko","ar","fr", "de","sl"]
+#        "bg","ca","cs","de","en","es", "fr","it","nl","no","ro","ru"]
 
+'''
 UD_LANG_TO_DIR = {
     "bg": "/UD_Bulgarian-BTB/bg_btb-ud-{split}.conllu",
     "ca": "/UD_Catalan-AnCora/ca_ancora-ud-{split}.conllu",
@@ -40,4 +42,15 @@ UD_LANG_TO_DIR = {
     "no": "/UD_Norwegian-Bokmaal/no_bokmaal-ud-{split}.conllu",
     "ro": "/UD_Romanian-RRT/ro_rrt-ud-{split}.conllu",
     "ru": "/UD_Russian-SynTagRus/ru_syntagrus-ud-{split}.conllu",
+}
+'''
+LANG_TO_DIR = {
+    "en": "/en_ewt-r2.15/en_ewt-ud-{split}.conllu",
+    "ja": "/ja_gsd-r2.15/ja_gsd-ud-{split}.conllu",
+    "zh": "/zh_gsdsimp-r2.15/zh_gsdsimp-ud-{split}.conllu",
+    "ko": "/ko_gsd-r2.15/ko_gsd-ud-{split}.conllu",
+    "ar": "/ar_padt-r2.15/ar_padt-ud-{split}.conllu",
+    "fr": "/fr_gsd-r2.15/fr_gsd-ud-{split}.conllu",
+    "de": "/de_gsd-r2.15/de_gsd-ud-{split}.conllu",
+    "sl": "/sl_ssj-r2.15/sl_ssj-ud-{split}.conllu",
 }
