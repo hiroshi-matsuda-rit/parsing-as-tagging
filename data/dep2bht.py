159ee19027c88f8754c13a87df71419207c25fbe
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-03-21 18:43:23 +0900

    revise path

--- data/dep2bht.py
+++ data/dep2bht.py
@@ -296,11 +296,11 @@ if __name__ == "__main__":
         print(f"Processing {language}...")
         if language == "English":
             # path = os.path.dirname(repo_directory) + "/ptb/ptb_{split}_3.3.0.sd.clean"
-            path = os.path.dirname(repo_directory) + "/ptb/ptb.{split}.conllu",
+            path = os.path.dirname(repo_directory) + "/treebanks/ptb/ptb.{split}.conllu",
             paths = [path.format(split=split) for split in ["train", "dev", "test"]]
         elif language == "Chinese":
             # path = os.path.dirname(repo_directory) + "/ctb/{split}.ctb.conll"
-            path = os.path.dirname(repo_directory) + "/ctb5.1/ctb5.1.{split}.conllu",
+            path = os.path.dirname(repo_directory) + "/treebanks/ctb5.1/ctb5.1.{split}.conllu",
             paths = [path.format(split=split) for split in ["train", "dev", "test"]]
         # elif language in ["bg", "ca","cs","de","en","es","fr","it","nl","no","ro","ru"]:
         else:
