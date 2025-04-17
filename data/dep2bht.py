5468f80f8011b7a402b9ae03ba25b5f52b87a8f7
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-04-17 14:20:07 +0900

    revise path

--- data/dep2bht.py
+++ data/dep2bht.py
@@ -311,7 +311,7 @@ if __name__ == "__main__":
             paths = [path.format(split=split) for split in ["train", "dev", "test"]]
         elif language == "Chinese":
             # path = os.path.dirname(repo_directory) + "/ctb/{split}.ctb.conll"
-            path = os.path.dirname(repo_directory) + "/data/ctb5.1/cht5.1-ud-{split}.conllu"
+            path = os.path.dirname(repo_directory) + "/data/ctb5.1/ctb5.1-ud-{split}.conllu"
             paths = [path.format(split=split) for split in ["train", "dev", "test"]]
         # elif language in ["bg", "ca","cs","de","en","es","fr","it","nl","no","ro","ru"]:
         else:
