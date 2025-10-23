913a4ad34cddeaabc87a7ea90d85101b841ded95
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-10-24 03:53:10 +0900

    filter_by_token_length.py

---
+++ filter_by_token_length.py
@@ -0,0 +1,29 @@
+import sys
+
+from transformers import AutoTokenizer
+
+
+def main():
+    model_path = sys.argv[1]
+    max_length = int(sys.argv[2])
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    lines = []
+    words = []
+    for _ in sys.stdin():
+        _ = _.rstrip("\n")
+        lines.append(_)
+        if _:
+            words.append(_.split("\t")[1])
+            continue
+        text = " ".join(words)
+        tokens = tokenizer.enocde(text)
+        if len(tokens) <= max_length:
+            print(*lines, sep="\n")
+        else:
+            print(*lines, sep="\n", file=sys.stderr)
+        lines = []
+        words = []
+
+
+if __name__ == "__main__":
+    main()
