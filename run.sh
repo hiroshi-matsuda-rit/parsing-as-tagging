ff9739e72729c89c6e51034543b61951767c7289
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2025-10-29 03:46:10 +0900

    improve

--- run.sh
+++ run.sh
@@ -13,6 +13,9 @@ fi
 model_path_name=${model#*/}
 # python run.py vocab --lang ${lang} --tagger hexa
 for r in `seq ${runs}` ; do
+  echo ${lang}-${model_path_name}.$r train
   python run.py train --lang ${lang} --max-depth 6 --tagger hexa --model bert --epochs 50 --batch-size ${train_batch_size} --lr 2e-5 --model-path ${model} --output-path checkpoints/${lang}-${model_path_name}.$r --use-tensorboard False &> log.${lang}-${model_path_name}.$r
+  echo ${lang}-${model_path_name}.$r evaluate
   python run.py evaluate --lang ${lang} --max-depth 10 --tagger hexa --bert-model-path ${model} --model-name ${lang}-hexa-bert-2e-05-50 --batch-size 64 --model-path checkpoints/${lang}-${model_path_name}.$r/ &>> log.${lang}-${model_path_name}.$r
+  echo ${lang}-${model_path_name}.$r finished
 done
